e97304869780566568d799cdb27567ed4868f8a7
kunal khairnar <kunalkhairnar61@gmail.com>
2024-04-24 17:58:07 +0530

    trainer solve the issue

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,40 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, connect } from 'react-redux';
 import classes from './Counter.module.css';
+import { Component } from 'react';
+import { INCREMENT } from '../store';
 
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
-
+  const show  = useSelector(state => state.showCounter)
 
   const incrementHandler =()=>{
-      dispatch({type:'IncrementBy5'})
+    console.log('updated state', counter)
+      dispatch({type:INCREMENT})
+  }
+
+  const increaseHandler=()=>{
+    console.log('updated increase state', counter)
+    dispatch({type:'Increase', amount:5})
   }
 
   const decrementHandler =()=>{
-      dispatch({type:'DecrementBy5'})
+    console.log('decrement handler',counter)
+      dispatch({type:'DecrementBy2'})
   }
 
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- {counter} --</div>
+      {show && <div className={classes.value}>-- {counter} --</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>IncrementBy5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -31,3 +43,47 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// class Counter extends Component{
+// // hooks are not use in classbase component
+//   incrementHandler(){
+//     this.props.increment()
+//   }
+//   decrementHandler(){
+//     this.props.decrement()
+//   }
+
+//   toggleCounterHandler(){
+
+//   }
+
+//   render(){
+//   return(
+//         <main className={classes.counter}>
+//             <h1>Redux Counter</h1>
+//             <div className={classes.value}>-- {this.counter} --</div>
+//             <div>
+//               <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//               <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//             </div>
+//             <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+//         </main>
+//     )
+//   }
+// }
+
+// const mapsStateProps = state => {
+//   return{
+//     counter: state.counter
+//   }
+// }
+
+// const mapDispatchProp = dispatch =>{
+//   return{
+//     increment:()=> dispatch({type:'IncrementBy5'}) ,
+//     decrement:()=> dispatch({type:'DecrementBy5'}) 
+//   }
+// }
+
+// export default connect(mapsStateProps, mapDispatchProp)(Counter);
+//connect is higher order component
--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,46 @@ import { createStore } from 'redux';
 
 //make a cardReducer function to use counting application
 
-const counterReducer = (state ={counter:0}, action)=>{
-        if(action.type === 'IncrementBy5'){
+export const INCREMENT = 'Increment'  //they will solve the potential issue in the react application
+// that will handle unexpected identifier this type of problems in bigger apps where is used multiple same identifier for updateing state
+// that type of problem solve new INCREMENT constant variable were is store identifiers.
+
+const initialState = {counter:0, showCounter:true}
+
+const counterReducer = (state = initialState, action)=>{
+        if(action.type === INCREMENT){
+            // state.counter++;
+            // //     console.log(state.counter)
+            // // return state;
+            console.log('previous state',state)            
             return {
-                counter : state.counter + 5
+                counter : state.counter + 2,
+                showCounter: state.showCounter
             }
         } 
-        
-        if(action.type === 'DecrementBy5'){
-            return {
-                counter : state.counter - 5
+
+        if(action.type === 'Increase'){
+            console.log('INCREASE HANDLER',state)
+            return{
+                counter : state.counter + action.amount,
+                showCounter: state.showCounter
             }
         }
+        
+        if(action.type === 'DecrementBy2'){
+            return {
+                counter : state.counter - 2,
+                showCounter: state.showCounter
+            }
+        }
+
+        if(action.type === 'toggle'){
+            return{
+                showCounter: !state.showCounter,
+                counter: state.counter
+            }
+        }
+
         return state;
 };
 
